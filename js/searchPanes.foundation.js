bf369ea15395301b66e57589cbd144fc0421c4cf
Allan Jardine <allan.jardine@sprymedia.co.uk>
2020-09-01 10:45:51 +0100

    dev: Made a mistake with the last commit, needs to be both uppercase rather than both lower case.
    Jira Issue DD-1644
    
    Sync to source repo @6fa7f6fc6f147187617960be83eee21fcc10a268

--- js/searchPanes.foundation.js
+++ js/searchPanes.foundation.js
@@ -14,7 +14,7 @@
             if (!$ || !$.fn.dataTable) {
                 $ = require('datatables.net-zf')(root, $).$;
             }
-            if (!$.fn.dataTable.searchpanes) {
+            if (!$.fn.dataTable.SearchPanes) {
                 require('datatables.net-searchpanes')(root, $);
             }
             return factory($, root, root.document);
