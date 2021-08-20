c4e5dfd36f9aa9c32f00f52a0718f93d6b8a5706
Allan Jardine <allan.jardine@sprymedia.co.uk>
2021-08-20 17:01:15 +0100

    652d71058ab33e96f2d61d2f918ddbd8dcb8f03d new: Add new options to allow the collapsing of individual SearchPanes
    Jira Issue DD-2109
    
    5206a819228e41ca7bb7c6fc82a0bd802b01ee66 dev: Add example for previous commit
    
    Jira Issue DD-2109
    
    Sync to source repo @5206a819228e41ca7bb7c6fc82a0bd802b01ee66

--- js/searchPanes.foundation.js
+++ js/searchPanes.foundation.js
@@ -47,8 +47,10 @@
     });
     $.extend(true, dataTable.SearchPanes.classes, {
         clearAll: 'dtsp-clearAll button secondary',
+        collapseAll: 'dtsp-collapseAll button secondary',
         disabledButton: 'disabled',
         panes: 'panes dtsp-panesContainer',
+        showAll: 'dtsp-showAll button secondary',
         title: 'dtsp-title'
     });
     return dataTable.searchPanes;
