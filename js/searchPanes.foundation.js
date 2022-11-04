4cb8cf794ecffae0cab92d8ef5cef5a22538ca7b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 11:42:34 +0000

    e43de6a84a29e2fbf692c2e1d06f97dce03cf9a2
    Sync to source repo @e43de6a84a29e2fbf692c2e1d06f97dce03cf9a2

--- js/searchPanes.foundation.js
+++ js/searchPanes.foundation.js
@@ -1,51 +1,6 @@
-
 /*! Bootstrap integration for DataTables' SearchPanes
  * © SpryMedia Ltd - datatables.net/license
  */
-
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net-zf', 'datatables.net-searchpanes'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		module.exports = function (root, $) {
-			if ( ! root ) {
-				// CommonJS environments without a window global must pass a
-				// root. This will give an error otherwise
-				root = window;
-			}
-
-			if ( ! $ ) {
-				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
-					require('jquery') :
-					require('jquery')( root );
-			}
-
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net-zf')(root, $);
-			}
-
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net-searchpanes')(root, $);
-			}
-
-
-			return factory( $, root, root.document );
-		};
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document, undefined ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
-
 $.extend(true, DataTable.SearchPane.classes, {
     buttonGroup: 'secondary button-group',
     disabledButton: 'disabled',
@@ -67,7 +22,3 @@ $.extend(true, DataTable.SearchPanes.classes, {
     showAll: 'dtsp-showAll button secondary',
     title: 'dtsp-title'
 });
-
-
-return DataTable;
-}));
